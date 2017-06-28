a635a1327f8b2f3f25653dff7c90bd7a9d7ed708
krizzmp <krizzmp@gmail.com>
2017-06-28 16:42:48 +0200

    Initial commit

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        this will be the structure
+      </div>
+    );
+  }
+}
+export default App
