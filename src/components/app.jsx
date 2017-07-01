83391ef8e756c47049d68e2a715c32cb5bda0bde
krizzmp <krizzmp@gmail.com>
2017-07-01 21:12:08 +0200

    add gihub auth and projects

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,21 +3,43 @@ import React from 'react'
 import { subscribe } from 'horizon-react'
 import { compose, withState, withHandlers } from 'recompose'
 import Messages from './messages'
+import Rx from 'rxjs'
 
-const App = (props) =>
+let k = observable => ({
+  watch() {
+    return observable
+  }
+})
+
+let log = (e, s) => {
+  console.log(e, s)
+  return e
+}
+const App = props =>
   <div>
-    {console.log(props.user)||''}
     <div className="center">
       <button onClick={props.sendMessage}>Send Message</button>
+      <button onClick={props.add}>add project</button>
+      <button onClick={props.connect}>connect project</button>
       <input onChange={props.handleChangeAuthor} />
       <input onChange={props.handleChangeText} />
     </div>
     <Messages chat={props.chat} />
-  </div>
+    <div className="user">
 
+      {log(props.h, 'h').map(u => <div>{u.name}</div>)}
+    </div>
+  </div>
 const mapDataToProps = {
   chat: hz => hz('messages'),
-  user: hz => hz.currentUser()
+  user: hz => hz.currentUser(),
+  h: hz =>
+    k(
+      hz.currentUser().watch()
+        .flatMap(us => hz('upc').findAll({ uid: us.id }).watch())
+        .flatMap(upcArr => Rx.Observable.from(upcArr))
+        .flatMap(upc => hz('projects').findAll({ id: upc.pid }).watch())
+    )
 }
 export default compose(
   withState('text', 'setText', ''),
@@ -35,6 +57,15 @@ export default compose(
     sendMessage: ({ horizon, reset, text, author }) => () => {
       reset()
       horizon('messages').store({ text, author })
+    },
+    connect: ({ horizon,user }) => () => {
+      horizon('upc').store({
+        uid: user[0],
+        pid: 'f4fd8137-a599-48a2-afb5-29898cea4e90'
+      })
+    },
+    add: ({ horizon }) => () => {
+      horizon('projects').store({ name: 'test' })
     }
   })
 )(App)
--- src/components/app2.jsx
+++ src/components/app2.jsx
@@ -4,14 +4,22 @@ import { Connector } from 'horizon-react'
 import App from './app'
 import { createStore } from 'redux'
 const Horizon = require('@horizon/client')
-
-const horizon = Horizon({ authType: 'anonymous' });
-console.log(horizon)
-horizon.connect()
-const store = createStore(s => s)
-const App2 = () =>
-  <Connector store={store} horizon={horizon}>
-    <App />
-  </Connector>
-
+let App2
+const horizon = Horizon({ authType: 'token' });
+if (!horizon.hasAuthToken()) {
+  console.log('hi')
+  App2 = () => (<div className="test">f</div>)
+  horizon.authEndpoint('github').subscribe((endpoint) => {
+    window.location.replace(endpoint);
+  });
+} else {
+  // We have a token already, do authenticated Horizon stuff here
+  console.log(horizon)
+  horizon.connect()
+  const store = createStore(s => s)
+  App2 = () =>
+    <Connector store={store} horizon={horizon}>
+      <App />
+    </Connector>
+}
 export default App2
